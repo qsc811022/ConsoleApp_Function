bd3269c30d2ca453c39a88fe5629d62bcdd22bde
劉敦桐 <zse811022@hotmail.com>
2023-09-04 20:44:32 +0800

    DAY3

--- Program.cs
+++ Program.cs
@@ -11,12 +11,35 @@ namespace ConsoleApp14
 		static void Main(string[] args)
 		{
 			MyMethod();
+
+			GreetUser("Ted");
+
+			int sum=AddNumbers(1,2);
+			Console.WriteLine(sum);
+
+
+			(int mySum, int myDifference) = AddAndSubtract(10, 5);
+			Console.WriteLine($"Sum: {mySum}, Difference: {myDifference}");
+
+
 		}
 
-		 static void MyMethod()
+		static void MyMethod()
 		{
 			// do something
 		}
 
+		static void GreetUser(string username)
+		{
+			Console.WriteLine("Hello, " + username);
+		}
+		static int AddNumbers(int a, int b)
+		{
+			return a + b;
+		}
+		static (int sum, int difference) AddAndSubtract(int a, int b)
+		{
+			return (a + b, a - b);
+		}
 	}
 }
